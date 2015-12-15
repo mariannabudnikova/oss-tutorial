f656cf87b7e5e499b1bf610643865f3bb4e44e47
Marianna <marianna.budnikova@gmail.com>
2015-12-15 06:06:25 -0700

    Added the incomplete python code for making a polyjuice potion. Snape will not be happy.

---
+++ potions.py
@@ -0,0 +1,27 @@
+def part1step1():
+	print "Add 3 measures of fluxweed to the cauldron insert into state (name) values(must have been picked on a full moon)."
+	print "Add 2 bundles of knotgrass to the cauldron."
+	print "Stir 3 times, clockwise."
+	print "Wave your wand then let potion brew for 80 minutes (for a Pewter Cauldron. A Brass Cauldron will only require 68, and a copper one only 60.)"
+
+def part1step2():
+	print "Add 4 leeches to the cauldron."
+	print "Add 2 scoops of lacewing flies to the mortar, crush to a fine powder, then add 2 measures of the crushed lacewings to the cauldron."
+	print "Heat for 30 seconds on a low heat."
+	print "Wave your wand to complete this stage of the potion."
+
+def part2step1():
+	print "Add 3 measures of boomslang skin to the cauldron."
+	print "Add 1 measure of bicorn horn to the mortar, crush to a fine powder, then add one measure of the crushed horn to the cauldron."
+	print" Heat for 20 seconds at a high temperature."
+	print "Wave your wand then let potion brew for 24 hours (for a Pewter Cauldron. A Brass Cauldron will only require 1224 minutes, and a copper one only 18 hours.)"
+
+def part2step2():
+	print "Add 1 additional scoop of lacewings to the cauldron."
+	print "Stir 3 times, anti-clockwise."
+	print "Split potion into multiple doses, if desired, then add the pieces of the person you wish to become."
+	print "Wave your wand to complete the potion."
+
+part1step2()
+part2step1()
+part2step2()
